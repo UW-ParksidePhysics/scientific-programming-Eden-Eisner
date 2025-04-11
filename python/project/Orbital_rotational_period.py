0c6364fdae5ee653574eaaeeded594f367400fb2
eisner <eisne003@rangers.uwp.edu>
2025-04-11 18:18:10 -0500

    Project work

--- python/project/Orbital_rotational_period.py
+++ python/project/Orbital_rotational_period.py
@@ -1,10 +1,12 @@
 """
 Use astronomical symbol for the sun
-Use a comparable size circle for the planet
+Use a comparable size circle for the planet to the sun
 arrow in the orbit itself
 use unit that has 2 sig fig for the periods w/o decimal place
 make 1.5 and 3 inch tall version
 """
+from matplotlib import pyplot as plt
+import numpy as np
 
 #### RENAME from project.py to (your_project_short_name).py
 # File structure
@@ -33,23 +35,69 @@ make 1.5 and 3 inch tall version
 #		data read-in
 #		simulation solution 
 #		visualization
-planet_data = {
-    "Mercury": {"orbital_period_days": 88, "spin_period_hours": 1407.6, "axial_tilt_degrees": 0.034},
-    "Venus": {"orbital_period_days": 225, "spin_period_hours": -5832.5, "axial_tilt_degrees": 177.4},
-    "Earth": {"orbital_period_days": 365.25, "spin_period_hours": 24, "axial_tilt_degrees": 23.44},
-    "Mars": {"orbital_period_days": 687, "spin_period_hours": 24.6, "axial_tilt_degrees": 25.19},
-    "Jupiter": {"orbital_period_days": 4331, "spin_period_hours": 9.9, "axial_tilt_degrees": 3.13},
-    "Saturn": {"orbital_period_days": 10747, "spin_period_hours": 10.7, "axial_tilt_degrees": 26.73},
-    "Uranus": {"orbital_period_days": 30589, "spin_period_hours": -17.2, "axial_tilt_degrees": 97.77},
-    "Neptune": {"orbital_period_days": 59800, "spin_period_hours": 16.1, "axial_tilt_degrees": 28.32},
-    "Ceres": {"orbital_period_days": 4375, "spin_period_hours": 9.1, "axial_tilt_degrees": 4.34},  # Ceres is a dwarf planet
-    "Pluto": {"orbital_period_days": 90560, "spin_period_hours": 153.3, "axial_tilt_degrees": 122.5}  # Pluto also a dwarf planet
-}
 
 
 # Negative spin periods mean the planet rotates in the opposite direction (retrograde rotation).
-def get_data(planet_name):
-    data = planet_data[planet_name]
-    return [planet_name] + list(data.values())
+def get_data():
+    planets_data = {
+        "Mercury": {"orbital_period_days": 88, "spin_period_hours": 1407.6, "axial_tilt_degrees": 0.034},
+        "Venus": {"orbital_period_days": 225, "spin_period_hours": -5832.5, "axial_tilt_degrees": 177.4},
+        "Earth": {"orbital_period_days": 365.25, "spin_period_hours": 24, "axial_tilt_degrees": 23.44},
+        "Mars": {"orbital_period_days": 687, "spin_period_hours": 24.6, "axial_tilt_degrees": 25.19},
+        "Jupiter": {"orbital_period_days": 4331, "spin_period_hours": 9.9, "axial_tilt_degrees": 3.13},
+        "Saturn": {"orbital_period_days": 10747, "spin_period_hours": 10.7, "axial_tilt_degrees": 26.73},
+        "Uranus": {"orbital_period_days": 30589, "spin_period_hours": -17.2, "axial_tilt_degrees": 97.77},
+        "Neptune": {"orbital_period_days": 59800, "spin_period_hours": 16.1, "axial_tilt_degrees": 28.32},
+        "Ceres": {"orbital_period_days": 4375, "spin_period_hours": 9.1, "axial_tilt_degrees": 4.34},
+        # Ceres is a dwarf planet
+        "Pluto": {"orbital_period_days": 90560, "spin_period_hours": 153.3, "axial_tilt_degrees": 122.5}
+        # Pluto also a dwarf planet
+    }
+    return planets_data
 
-print(get_data("Mercury"))
+
+def draw_sun():
+    plt.Circle((0,0),.25, color='k')
+    pass
+
+
+def draw_planet(planet_name):
+    from astronomical_symbols import astronomical_symbols
+
+
+    print(astronomical_symbols[planet_name])
+    pass
+
+
+def draw_orbit_with_arrow(ellipse_axes, ellipse_color = 'k'):
+    angle = np.linspace(0, 2*np.pi)
+    plt.plot(ellipse_axes[0] *np.cos(angle), ellipse_axes[1] * np.sin(angle), color = ellipse_color)
+    pass
+
+
+def draw_axis_with_arrow(axial_tilt, spin_period):
+    print(axial_tilt, spin_period)
+    return
+
+
+def draw_labels(label_data):
+    pass
+
+
+if __name__ == "__main__":
+    sizes = ((6,3),(3,1.5))
+    data = get_data()
+    planet = "Mercury"
+    reduced_data = {planet:data[planet]} # for testing specific planets
+    for planet, planet_data in reduced_data.items():
+        for size in sizes:
+            plt.subplots(figsize=size)
+            draw_sun()
+            draw_planet(planet)
+            draw_orbit_with_arrow([size[0]/2,size[1]/2])
+            draw_axis_with_arrow(planet_data["axial_tilt_degrees"], planet_data["spin_period_hours"])
+            draw_labels(planet_data)
+
+
+
+            plt.show()
